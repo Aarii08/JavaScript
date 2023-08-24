968d5b4bfefefdf582bed3a0915097231dace114
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-08-24 05:13:35 +0000

    Finish

---
+++ Weather-Lesson/script.js
@@ -0,0 +1,22 @@
+const API_KEY = "ae2c19307333577ac5890856d6adf434";
+
+$(function(){
+  $('#btn').on('click', function() {
+
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dateType : 'jsonp',
+    }).done(function (date) {
+      $('#place').text(date.name);
+      $('#temp_max').text(date.main.temp_max);
+      $('#temp_min').text(date.main.temp_min);
+      $('#humidity').text(date.main.humidity);
+      $('#speed').text(date.wind.speed);
+      $('#weather').text(date.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + date.weather[0].icon + ".png");
+      $('img').attr("alt",date.weather[0].main)
+    }).fail(function (date) {
+      alert('通信に失敗しました。');
+    });
+  });
+});
